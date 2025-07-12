2b53b70af8c539d412a843061ffd025744542c72
KunalMalviya <kunalbhurajimalviya5@gmail.com>
2025-07-12 13:44:32 +0530

    connect frontend [cors]

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
@@ -10,6 +11,11 @@ connectDB();
 
 const app = express();
 app.use(express.json());
+
+app.use(cors({
+  origin: '*',
+}));
+
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
